bcb2bf4f2160367f799ea9453e2e38a69cc5ef0b
Julie Van Audenhaege <julie.vanaudenhaege@hotmail.com>
2022-01-18 15:23:37 +0100

    done

---
+++ ex02/main.cpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvan-aud <rvan-aud@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/18 14:18:17 by rvan-aud          #+#    #+#             */
+/*   Updated: 2022/01/18 15:23:03 by rvan-aud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mutantstack.hpp"
+
+int	main(void)
+{
+	MutantStack<int> mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+
+	MutantStack<int>::iterator	it = mstack.begin();
+	MutantStack<int>::iterator	ite = mstack.end();
+
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+
+	std::stack<int> s(mstack);
+	return (0);
+}
